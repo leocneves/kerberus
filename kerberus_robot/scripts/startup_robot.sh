0c43237908640904f460e0a6d809c631ebc1e081
pi robot <neves.leonardoc@gmail.com>
2022-07-20 13:56:43 -0300

    some config files and vosk launch

--- kerberus_robot/scripts/startup_robot.sh
+++ kerberus_robot/scripts/startup_robot.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-export ROS_MASTER_URI=http://192.168.15.37:11311
-export ROS_IP=192.168.15.37
-export ROS_USERNAME=192.168.15.37
+export ROS_MASTER_URI=http://192.168.15.3:11311
+export ROS_IP=192.168.15.3
+export ROS_USERNAME=192.168.15.3
 echo "##### ENV VAR UPDATED! #####"
 
-sh -c "echo nameserver 8.8.8.8 > /etc/resolv.conf"
+#sh -c "echo nameserver 8.8.8.8 > /etc/resolv.conf"
 
 echo "######### ARRUMA DNS ############"
 
-cd /home/ubuntu/catkin_ws/src/kerberus
+cd /home/pi/catkin_ws/src/kerberus
 git pull
+
 echo "##### REPOSITORY UPDATED! #####"
 
-source /opt/ros/kinetic/setup.bash
-source /home/ubuntu/catkin_ws/devel/setup.bash
+source /opt/ros/melodic/setup.bash
+source /home/pi/catkin_ws/devel/setup.bash
 
 echo "##### ENV SOURCED! #####"
 

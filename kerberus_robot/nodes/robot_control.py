51e07b6b50db936943e13cc8eb98141c1d036a04
leocneves <neves.leonardoc@gmail.com>
2022-07-20 15:52:13 -0300

    fixed python3 issue

--- kerberus_robot/nodes/robot_control.py
+++ kerberus_robot/nodes/robot_control.py
@@ -6,6 +6,7 @@ import smach
 import smach_ros
 from std_msgs.msg import String
 import time
+from geometry_msgs.msg import Twist
 
 # define state startTalk
 class StartTalk(smach.State):
@@ -25,47 +26,101 @@ class StartTalk(smach.State):
         global pub_sound
         rospy.loginfo('Executing state startTalk')
         t1 = time.time()
-        while (time.time() - t1) <= 60:
+        while (time.time() - t1) <= 60 * 3:
             if self.got:
                 self.got = False
-                pub_sound.publish("Yes")
+                pub_sound.publish(", Yes")
                 return 'finished'
+        pub_sound.publish(", Is anyone there?")
         return 'failed'
 
 # define state Bar
-class Test(smach.State):
+class Action(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['finished'])
+        smach.State.__init__(self, outcomes=['finished', 'failed'])
+        self.got = False
+
+        self.subscriber = rospy.Subscriber('/speech_recognition/final_result', String, self.callback)
+
+        self.actions = ['walk forward', 'turn right', 'turn left', 'walk backwards']
 
     def execute(self, userdata):
-        global pub_sound
-        rospy.loginfo('Executing state TEST')
-        time.sleep(2)
-        pub_sound.publish("Nice, I can hear you very well Leonardo")
+        global pub_sound, velocity_publisher
 
-        return 'finished'
+        rospy.loginfo('Executing state Action')
+
+        vel_msg = Twist()
+        t1 = time.time()
+
+        # Waiting for action up to
+        while ((time.time() - t1) <= 10) or self.got:
+            if self.got:
+                ############## ACTIONS ##################
+                pub_sound.publish(", ok")
+
+                c = time.time()
+                n = 3
+                if self.data == 'walk forward':
+                    vel_msg.linear.x = 1.0
+                    vel_msg.angular.z = 0.0
+                    while (time.time() - c) < n:
+                        velocity_publisher.publish(vel)
+                elif self.data == 'walk backwards':
+                    vel_msg.linear.x = -1.0
+                    vel_msg.angular.z = 0.0
+                    while (time.time() - c) < n:
+                        velocity_publisher.publish(vel)
+                elif self.data == 'turn right':
+                    vel_msg.linear.x = 0.0
+                    vel_msg.angular.z = 1.0
+                    while (time.time() - c) < n:
+                        velocity_publisher.publish(vel)
+                elif self.data == 'turn left':
+                    vel_msg.linear.x = 0.0
+                    vel_msg.angular.z = -1.0
+                    while (time.time() - c) < n:
+                        velocity_publisher.publish(vel)
 
 
+                vel_msg.linear.x = 0.0
+                vel_msg.angular.z = 0.0
+                velocity_publisher.publish(vel)
 
+
+                self.got = False
+                return 'finished'
+        pub_sound.publish(", Sorry, I don't understand")
+        return 'failed'
+
+
+    def callback(self, data):
+        self.data = data.data
+        if data.data in self.actions:
+            self.got = True
+        else:
+            self.got = False
 
 
 def main():
-    global pub_sound
+    global pub_sound, velocity_publisher
     rospy.init_node('control_state_machine')
 
     pub_sound = rospy.Publisher('/tts/phrase', String, queue_size=1)
+    velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
     # Create a SMACH state machine
     sm = smach.StateMachine(outcomes=['outcome4'])
+
     # Open the container
     with sm:
         # Add states to the container
         smach.StateMachine.add('ROBOT', StartTalk(),
-                               transitions={'finished':'TEST', 'failed':'ROBOT'})
-        smach.StateMachine.add('TEST', Test(),
-                               transitions={'finished':'ROBOT'})
+                               transitions={'finished':'ACTION', 'failed':'ROBOT'})
+        smach.StateMachine.add('ACTION', Action(),
+                               transitions={'finished':'ROBOT', 'failed':'ROBOT'})
 
     # Execute SMACH plan
+    pub_sound.publish(", I am alive")
     outcome = sm.execute()
 
 
